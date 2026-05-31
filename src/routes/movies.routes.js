e440688bea40b3d0a18b3c90a581159ae1b9ba76
Fatima <ftshn84@gmail.com>
2026-05-31 14:02:46 +0200

    Refactor movie routes: comment out response logic for GET and POST endpoints

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -3,24 +3,12 @@ const router = express.Router();
 const movies = require("../data/movies");
 
 router.get("/", (req, res) => {
-  res.json(movies);
+  // TODO: return all movies
+  console.log("test")
 });
 
 router.post("/", (req, res) => {
-  const { title } = req.body;
-
-  if (typeof title !== "string" || title.trim() === "") {
-    return res.status(400).json({ error: "title is required" });
-  }
-
-  const newMovie = {
-    id: movies.length > 0 ? movies[movies.length - 1].id + 1 : 1,
-    title: title.trim(),
-  };
-
-  movies.push(newMovie);
-
-  return res.status(201).json(newMovie);
+  // console.log("test")
 });
 
 module.exports = router;
